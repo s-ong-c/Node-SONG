b30602c57f2ac70923e512315b5b8c80e554f08a
s-ong-c <somony9292@gmail.com>
2018-09-14 20:14:12 +0900

    passport 이론정리

---
+++ node-pass/main.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet')
+app.use(helmet());
+var session = require('express-session')
+var FileStore = require('session-file-store')(session)
+var flash = require('connect-flash');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(compression());
+app.use(session({
+  secret: 'asadlfkj!@#!@#dfgasdg',
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore()
+}))
+app.use(flash());
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  {
+    usernameField:'email',
+    passwordField:'pwd'
+  },
+  function(username, password, done) {
+    console.log('LocalStrategy',username,password );
+
+    /*
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+    */
+  }
+));
+
+
+
+
+
+app.post('/auth/login_process',
+    passport.authenticate('local',
+    { successRedirect: '/',
+      failureRedirect: '/auth/login'
+    }));
+
+app.get('*', function (request, response, next) {
+  fs.readdir('./data', function (error, filelist) {
+    request.list = filelist;
+    next();
+  });
+});
+
+var indexRouter = require('./routes/index');
+var topicRouter = require('./routes/topic');
+var authRouter = require('./routes/auth')(passport);
+
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
+app.use('/auth', authRouter);
+
+app.use(function (req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
